0f1cb540699cc455105f40994c0bb46b82c56f42
Abnormally <darkling.pk@gmail.com>
2017-02-06 07:30:24 +0200

    Create water.c

---
+++ CS50/pset01/water.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+ 
+int read(char word[]) {
+    char x; int i;
+ 
+    for (i = 0;;i++) {
+        x = getchar();
+        if (x == EOF || x == ' ' || x == '\n') break;
+        word[i] = x;
+    }
+ 
+    return i;
+}
+ 
+int get_int();
+ 
+int retry() {
+    printf("Retry: ");
+    return get_int();
+}
+ 
+int get_int() { // Вместо get_int из cs50.h
+    char word[50]; int len, i, result = 0;
+    len = read(word);
+ 
+    if (len == 0) return retry();
+ 
+    for (i = 0; i < len; i++) {
+        if (word[i] < '0' || word[i] > '9') return retry();
+        result = result * 10 + (word[i] - 0x30);
+    }
+ 
+    return result;
+}
+ 
+int main(void) {
+    printf("minutes: ");
+    int minutes = get_int();
+    printf("bottles: %i\n", (minutes * 12));
+}
